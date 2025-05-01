887d07537694c63f8957f3a2ddc6a00d627144fa
Dominic Sisneros <dsisnero@gmail.com>
2025-05-01 10:03:42 -0600

    refactor: remove redundant module nesting in cli_tester.cr

--- src/cli_tester.cr
+++ src/cli_tester.cr
@@ -10,7 +10,7 @@ require "./cli_tester/output_normalizer"
 require "./cli_tester/mock_adapter"
 require "./cli_tester/snapshot"
 require "./cli_tester/shell"
-require "./cli_tester/shard_binary"      # Added
+require "./cli_tester/shard_binary" # Added
 
 # CliTester provides utilities for end-to-end testing of CLI applications.
 #
@@ -42,35 +42,34 @@ module CliTester
   #   result.stdout.should eq("content")
   # end
   # ```
-  module CliTester
-    # Automatically configure ShardBinary when the module is loaded.
-    # This finds the project's root directory and shard.yml file.
-    ShardBinary.configure
+  # Automatically configure ShardBinary when the module is loaded.
+  # This finds the project's root directory and shard.yml file.
+  ShardBinary.configure
 
-    # The root directory of the project being tested (where shard.yml was found).
-    # Set automatically by `ShardBinary.configure`.
-    class_property root_dir : String = ShardBinary.root_dir
+  # The root directory of the project being tested (where shard.yml was found).
+  # Set automatically by `ShardBinary.configure`.
+  class_property root_dir : String = ShardBinary.root_dir
 
-    # The path to the shard.yml file of the project being tested.
-    # Set automatically by `ShardBinary.configure`.
-    class_property shard_file : String = ShardBinary.shard_file
+  # The path to the shard.yml file of the project being tested.
+  # Set automatically by `ShardBinary.configure`.
+  class_property shard_file : String = ShardBinary.shard_file
 
-    # Sets up a temporary environment for a CLI test, yields it to the block,
-    # and ensures cleanup afterwards.
-    #
-    # @yield [Environment] Provides access to the test environment
-    #
-    # Example:
-    # ```
-    # CliTester.test do |env|
-    #   env.write_file("test.txt", "content")
-    #   result = env.execute("cat test.txt")
-    #   result.stdout.should eq("content")
-    # end
-    # ```
-    def self.test(& : Environment -> _)
-      env = Environment.new
-      begin
+  # Sets up a temporary environment for a CLI test, yields it to the block,
+  # and ensures cleanup afterwards.
+  #
+  # @yield [Environment] Provides access to the test environment
+  #
+  # Example:
+  # ```
+  # CliTester.test do |env|
+  #   env.write_file("test.txt", "content")
+  #   result = env.execute("cat test.txt")
+  #   result.stdout.should eq("content")
+  # end
+  # ```
+  def self.test(& : Environment -> _)
+    env = Environment.new
+    begin
       env.chdir do
         yield env
       end
